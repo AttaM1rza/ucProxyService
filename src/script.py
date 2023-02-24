c858a37876628f2432c08ff4d56d67ea46461410
Atta Mirza <atta.mirza@outlook.de>
2023-02-24 23:01:02 +0100

    creating codebase

--- src/script.py
+++ src/script.py
@@ -12,10 +12,7 @@ import undetected_chromedriver as uc
 from config import verificationImg, dataFolder
 
 sleepTime = 6
-url = f"{sys.argv[1:]}"
-logging.info(url)  # "https://www.zara.com/de/de/jacke-aus-kunstleder-p08281450.html?v1=222756772"
-logging.info(type(url))
-url = "https://www.zara.com/de/de/jacke-aus-kunstleder-p08281450.html?v1=222756772"
+
 VERSION_MAIN = 110
 
 
@@ -35,6 +32,12 @@ logging.getLogger("parso").setLevel(100)
 # o = uc.ChromeOptions()
 # o.arguments.extend(["--no-sandbox", "--disable-setuid-sandbox"])  # these are needed to run chrome as root
 driver = uc.Chrome(advanced_elements=True, version_main=VERSION_MAIN)
+
+url = f"{sys.argv[1:]}"
+logging.info(url)  # "https://www.zara.com/de/de/jacke-aus-kunstleder-p08281450.html?v1=222756772"
+logging.info(type(url))
+url = "https://www.zara.com/de/de/jacke-aus-kunstleder-p08281450.html?v1=222756772"
+
 driver.get(url)
 logging.getLogger().info(f'sleeping {sleepTime} seconds to give site a chance to load')
 time.sleep(sleepTime)  # this is only for the timing of the screenshot
