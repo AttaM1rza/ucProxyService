3830c0810f8d195c01deeaf5551e556edd3d4c5d
Atta Mirza <atta.mirza@outlook.de>
2023-02-24 19:06:18 +0100

    creating codebase

--- src/script.py
+++ src/script.py
@@ -15,33 +15,46 @@ url = "https://www.zara.com/de/de/jacke-aus-kunstleder-p08281450.html?v1=2227567
 VERSION_MAIN = 110
 
 
-def saveHtmlFile(destinationDir: str, htmlResponse):
-    filename = datetime.now().strftime('%d-%m-%Y_%H-%M-%S') + ".html"
-    savePath = os.path.join(destinationDir, filename)
+class UCProxyBrowser:
+    driver = None
 
-    with open(savePath, "w", encoding="utf-8") as file:
-        try:
-            file.write(htmlResponse.text)
-        except AttributeError:
-            # AttributeError: 'str' object has no attribute 'text'
-            file.write(htmlResponse)
+    def __init__(self):
+        self.bootUpBrowser()
+        self.executeBrowsing()
+        self.shutDownBrowser()
+        pass
 
+    def bootUpBrowser(self):
+        logging.basicConfig(level=10)
+        logging.getLogger("parso").setLevel(100)
+        # o = uc.ChromeOptions()
+        # o.arguments.extend(["--no-sandbox", "--disable-setuid-sandbox"])  # these are needed to run chrome as root
+        self.driver = uc.Chrome(advanced_elements=True, version_main=VERSION_MAIN)
 
-logging.basicConfig(level=10)
-logging.getLogger("parso").setLevel(100)
+    def executeBrowsing(self):
+        self.driver.get(url)
+        logging.getLogger().info(f'sleeping {sleepTime} seconds to give site a chance to load')
+        time.sleep(sleepTime)  # this is only for the timing of the screenshot
+        logging.getLogger().setLevel(20)
+        self.driver.save_screenshot(verificationImg)
+        subprocess.run(["catimg", verificationImg])
+        logging.getLogger().info(f'screenshot saved to {verificationImg}')
+        sourcePage = self.driver.page_source
+        self.saveHtmlFile(dataFolder, sourcePage)
+        logging.getLogger().info(f'source page saved to {dataFolder}')
+        # input("press a key to quit")
 
-# o = uc.ChromeOptions()
-# o.arguments.extend(["--no-sandbox", "--disable-setuid-sandbox"])  # these are needed to run chrome as root
-driver = uc.Chrome(advanced_elements=True, version_main=VERSION_MAIN)  # driver = uc.Chrome( version_main = 108 )
-driver.get(url)
+    def shutDownBrowser(self):
+        exit()
+        pass
 
-logging.getLogger().info(f'sleeping {sleepTime} seconds to give site a chance to load')
-time.sleep(sleepTime)  # this is only for the timing of the screenshot
-logging.getLogger().setLevel(20)
-driver.save_screenshot(verificationImg)
-subprocess.run(["catimg", verificationImg])
-logging.getLogger().info(f'screenshot saved to {verificationImg}')
-saveHtmlFile(dataFolder, driver.page_source)
-logging.getLogger().info(f'source page saved to {dataFolder}')
-# input("press a key to quit")
-exit()
+    def saveHtmlFile(self, destinationDir: str, htmlResponse):
+        filename = datetime.now().strftime('%d-%m-%Y_%H-%M-%S') + ".html"
+        savePath = os.path.join(destinationDir, filename)
+
+        with open(savePath, "w", encoding="utf-8") as file:
+            try:
+                file.write(htmlResponse.text)
+            except AttributeError:
+                # AttributeError: 'str' object has no attribute 'text'
+                file.write(htmlResponse)
