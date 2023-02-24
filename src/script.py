726599e491b6dd176a4cd4d288a61fb227cddcd1
Atta Mirza <atta.mirza@outlook.de>
2023-02-24 19:12:02 +0100

    creating codebase

--- src/script.py
+++ src/script.py
@@ -14,6 +14,23 @@ url = "https://nowsecure.nl/#relax"
 url = "https://www.zara.com/de/de/jacke-aus-kunstleder-p08281450.html?v1=222756772"
 VERSION_MAIN = 110
 
+logging.basicConfig(level=10)
+logging.getLogger("parso").setLevel(100)
+# o = uc.ChromeOptions()
+# o.arguments.extend(["--no-sandbox", "--disable-setuid-sandbox"])  # these are needed to run chrome as root
+driver = uc.Chrome(advanced_elements=True, version_main=VERSION_MAIN)
+driver.get(url)
+logging.getLogger().info(f'sleeping {sleepTime} seconds to give site a chance to load')
+time.sleep(sleepTime)  # this is only for the timing of the screenshot
+logging.getLogger().setLevel(20)
+driver.save_screenshot(verificationImg)
+subprocess.run(["catimg", verificationImg])
+logging.getLogger().info(f'screenshot saved to {verificationImg}')
+sourcePage = driver.page_source
+logging.getLogger().info(f'source page saved to {dataFolder}')
+# input("press a key to quit")
+exit()
+
 
 class UCProxyBrowser:
     driver = None
